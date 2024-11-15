994266c570bc90fda4ba0b7e22ba9a8c6f8e2bd8
Md. Mahfuzur Rahman <mahfuzur.rahman@brainstation-23.com>
2024-11-15 17:51:28 +0600

    API base url is being resolved from env

--- data/lib/remote/api_client/movie_api_client.dart
+++ data/lib/remote/api_client/movie_api_client.dart
@@ -1,9 +1,10 @@
 import 'package:data/local/shared_preference/entity/user_session_shared_pref_entity.dart';
 import 'package:data/remote/api_client/api_client.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class MovieApiClient extends ApiClient {
   @override
-  String get baseUrl => "https://yts.mx/api/v2";
+  String get baseUrl => dotenv.env['API_BASE_URL']!;
 
   @override
   Future<Map<String, String>> getCustomHeader() async {
